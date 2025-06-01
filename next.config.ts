9053c2206242630a021825c95f58e0d3cafda4a2
ak-akkinapelli <akkinapelliakshaykumar@gmail.com>
2025-06-01 21:40:39 +0530

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   trailingSlash: true,
   images: {
     unoptimized: true, // ✅ Ensures Cloudflare handles images properly
   },
-  output: "standalone", // ✅ Required for running Next.js in Workers
+  output: "export", // ✅ Required for static exports
 };
 
 module.exports = nextConfig;
