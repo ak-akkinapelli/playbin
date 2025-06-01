e30dcefab29f1d7cda9dd40c385cdd52cfca88d2
ak-akkinapelli <akkinapelliakshaykumar@gmail.com>
2025-06-01 17:20:10 +0530

    Fixed Worker script & ESLint errors

--- src/worker.js
+++ src/worker.js
@@ -1,7 +1,10 @@
-export default {
+const worker = {
   async fetch(request) {
+    console.log(`Received request for: ${request.url}`);
     return new Response("Cloudflare Worker running!", {
       headers: { "Content-Type": "text/plain" },
     });
   },
 };
+
+export default worker;
