888428b0975013656fca92736878299b619bf7a4
ak-akkinapelli <akkinapelliakshaykumar@gmail.com>
2025-06-01 17:01:46 +0530

    removed unsued vars

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 // app/page.tsx
 'use client'
 
-import {  useState } from 'react'
 import Songs from './components/Songs'
 
 export default function Home() {
@@ -12,7 +11,6 @@ export default function Home() {
       <div className="max-w-6xl mx-auto space-y-8">
         <h1 className="text-4xl font-bold mb-8">🎵 My Music Player</h1>
 
-        {/* R2 Songs Section */}
         <Songs/>
       </div>
     </main>
